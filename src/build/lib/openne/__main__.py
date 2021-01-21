2ce66994edfba0e6cecff00b9b290c067b9fdbf7
Quo <qhuozhihao@icloud.com>
2021-01-21 13:07:32 +0800

    added 10 times evaluations(5-fold did not finished)

---
+++ src/build/lib/openne/__main__.py
@@ -0,0 +1,238 @@
+from __future__ import print_function
+
+import time
+import ast
+
+import numpy as np
+import random
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+import datetime
+from . import tasks, dataloaders, models
+
+def ListInput(s: str):
+    l = list(ast.literal_eval(s))
+    if type(l) is not list:
+        raise TypeError
+    return l
+
+def xtype(val):
+    if type(val) is str:
+        return str.lower
+    if type(val) is list:
+        return ListInput
+    return type(val)
+
+def toargstr(s):
+    if s[:2] != '--':
+        s = '--' + s
+    s = s.replace('_', '-')
+    return s
+
+def legal_arg_name(arg):
+    if arg[0] == '_':
+        return False
+    return True
+def getSum(a,b):
+    for key in b:
+        if key not in a:
+            return b
+        else:
+            a[key]=a[key]+b[key]
+    return a
+def getAverage(b,num):
+    for key in b:
+        b[key]=b[key]/num
+    return b
+def addarg(arg, group, used_names, val, default=False, hlp=None, choices=None):
+    kwargs = {}
+    if arg not in used_names and legal_arg_name(arg):
+        used_names.add(arg)
+        if xtype(val) is bool and default:
+            if val:
+                if 'dest' not in kwargs:
+                    kwargs['dest'] = arg
+                arg = 'no_' + arg
+                kwargs['action'] = 'store_false'
+                if hlp is not None:
+                    kwargs['help'] = hlp + " (action {}, dest={})".format(kwargs['action'], kwargs['dest'])
+                else:
+                    kwargs['help'] = "(action {}, dest={})".format(kwargs['action'], kwargs['dest'])
+            else:
+                kwargs['action'] = 'store_true'
+                if hlp is not None:
+                    kwargs['help'] = hlp + " (action {})".format(kwargs['action'])
+                else:
+                    kwargs['help'] = '(action {})'.format(kwargs['action'])
+
+        else:
+            if val is not None:
+                kwargs['type'] = xtype(val)
+            if default:
+                kwargs['default'] = val
+                if hlp is None:
+                    kwargs['help'] = ' '
+                    kwargs['help'] += '(default: {})'.format(val)
+                else:
+                    kwargs['help'] = '(default: {})'.format(val)
+            if hlp is not None:
+                kwargs['help'] = hlp
+        if choices and arg in choices:
+            kwargs['choices'] = choices[arg]
+        group.add_argument(toargstr(arg), **kwargs)
+        return True
+    elif legal_arg_name(arg):
+        return False
+    return True
+
+
+def parse_args():
+    parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter,
+                            conflict_handler='resolve')
+
+    # tasks, models, dataloaders
+    parser.add_argument('--task', choices=tasks.taskdict.keys(), type=str.lower,
+                        help='Assign a task. If unassigned, OpenNE will '
+                             'automatically assign one according to the model.')
+    parser.add_argument('--model', choices=models.modeldict.keys(), type=str.lower,
+                        help='Assign a model.', required=True)
+    datasetgroup = parser.add_mutually_exclusive_group(required=True)
+    datasetgroup.add_argument('--dataset', choices=dataloaders.datasetdict.keys(), type=str.lower,
+                              help='Assign a dataset as provided by OpenNE. '
+                                   'Use --local-dataset if you want to load dataset from file.')
+
+    # self-defined dataset
+    local_inputs = parser.add_argument_group('LOCAL DATASET INPUTS')
+    datasetgroup.add_argument('--local-dataset', action='store_true',
+                              help='Load dataset from file. Check LOCAL DATASET INPUTS for more details.')
+    local_inputs.add_argument('--root-dir', help='Root directory of input files. If empty, you should provide '
+                                                 'absolute paths for graph files.',
+                              default=None)
+    local_input_format = local_inputs.add_mutually_exclusive_group()
+    local_input_format.add_argument('--edgefile', help='Graph description in edgelist format.')
+    local_input_format.add_argument('--adjfile', help='Graph description in adjlist format.')
+    local_inputs.add_argument('--labelfile', help='Node labels.')
+    local_inputs.add_argument('--features', help='Node features.')
+    local_inputs.add_argument('--status', help="Dataset status.")
+    local_inputs.add_argument('--name', help="Dataset name.", default='SelfDefined')
+    local_inputs.add_argument('--weighted', action='store_true', help='View graph as weighted. (action store_true)')
+    local_inputs.add_argument('--directed', action='store_true', help='View graph as directed. (action store_true)')
+
+    used_names = set()
+    choices = {'measurement': ('katz', 'cn', 'rpr', 'aa')}
+    # structure & training args
+    generalgroup = parser.add_argument_group("GENERAL MODEL ARGUMENTS")
+    no_default_args = ['epochs', 'output',]
+    addarg("clf_ratio", generalgroup, used_names, 0.5, True)
+    generalgroup.add_argument('--validate', type=bool)
+    model_args = models.ModelWithEmbeddings.args()
+    for arg in model_args:
+        addarg(arg, generalgroup, used_names, model_args[arg], arg not in no_default_args, choices=choices)
+
+    simpledict = models.modeldict.copy()
+    simpledict.__delitem__('node2vec')
+    simpledict.__delitem__('deepwalk')
+    simpledict.__delitem__('gf')
+    simpledict.__delitem__('lap')
+    simpledict['node2vec & deepwalk'] = models.Node2vec
+
+    # add duplicate args as general model args
+    general_names = used_names.copy()
+    tmp_used_names = {}
+    addarg('sparse', generalgroup, used_names, False, True, '(in lle, gcn)')
+    for modelname in simpledict:
+        model = simpledict[modelname]
+        model_args = model.args()
+        for arg in model_args:
+            if arg in tmp_used_names:
+                tmp_used_names[arg][0].append(modelname)
+            else:
+                tmp_used_names[arg] = ([modelname], model_args[arg])
+    for arg, (modelnames, argval) in tmp_used_names.items():
+        if len(modelnames) > 1:
+            addarg(arg, generalgroup, used_names, argval, False, '(in {})'.format(', '.join(modelnames)), choices)
+
+    for modelname in simpledict:
+        model = simpledict[modelname]
+        modelgroup = parser.add_argument_group(modelname.upper())
+        shared_params = []
+        model_args = model.args()
+        for arg in model_args:
+            if not addarg(arg, modelgroup, used_names, model_args[arg], True, None, choices=choices) \
+                    and arg not in general_names:
+                argval = model_args[arg]
+                paramdescript = ' ' + toargstr(arg)
+                if len(paramdescript) < 22:
+                    paramdescript += ' ' * (22 - len(paramdescript))
+                else:
+                    paramdescript += '\n' + ' ' * 22
+                paramdescript += '(default: {})'.format(argval)
+                shared_params.append(paramdescript)
+        if shared_params:
+            modelgroup.description = 'Shared params:\n{}'.format(' \n'.join(shared_params))
+
+    args = parser.parse_args()
+
+    return args
+
+
+def parse(**kwargs):
+    if 'dataset' in kwargs:
+        Graph = dataloaders.datasetdict[kwargs['dataset']]
+    else:
+        name_dict = {k: v for k,v in kwargs.items() if k in ['edgefile', 'adjfile', 'labelfile', 'features', 'status']}
+        Graph = dataloaders.create_self_defined_dataset(kwargs['root_dir'], name_dict, kwargs['name'],
+                                                          kwargs['weighted'], kwargs['directed'], 'features' in kwargs)
+    Model = models.modeldict[kwargs['model']]
+    taskname = kwargs.get('task', None)
+    if taskname is None:
+        if Model in tasks.supervisedmodels:
+            Task = tasks.SupervisedNodeClassification
+        else:
+            Task = tasks.UnsupervisedNodeClassification
+    else:
+        Task = tasks.taskdict[taskname]
+    return Task, Graph, Model
+
+
+def main(args):
+
+    # parsing
+    args = {x: y for x, y in args.__dict__.items() if y is not None}
+
+    print("actual args:",args)
+
+    Task, Graph, Model = parse(**args)  # parse required Task, Dataset, Model (classes)
+    dellist = ['dataset', 'edgefile', 'adjfile', 'labelfile', 'features',
+               'status', 'weighted', 'directed', 'root_dir', 'task', 'model']
+    for item in dellist:
+        if item in args:
+            args.__delitem__(item)
+    # preparation
+    task = Task(**args)                 # prepare task
+    task.check(Model, Graph)          # check parameters
+    train_args = task.kwargs
+    model = Model(**train_args)               # prepare model
+    graph = Graph()                 # prepare dataset
+
+    res = task.train(model, graph)    # train
+
+    # evaluation
+    starttime = datetime.datetime.now()
+    resultMap={}
+    num=10
+    for i in range(num):
+        print("[",str(i+1),'/',num,"]")
+        temp=task.evaluate(model, res, graph)  # evaluate
+        resultMap=getSum(resultMap,temp)
+    resultMap=getAverage(resultMap,num)
+    print('The final result is ')
+    print(resultMap)
+    endtime = datetime.datetime.now()
+    print('Finished evaluation. Time used = ',(endtime - starttime).seconds,'s')
+
+
+
+if __name__ == "__main__":
+    random.seed(32)
+    np.random.seed(32)
+    main(parse_args())
---
+++ src/openne/models/vgae.py
@@ -0,0 +1,203 @@
+import numpy as np
+from .gcn.utils import *
+from .gcn import gcnModel
+from .gcn.layers import *
+from .models import *
+import time
+import scipy.sparse as sp
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class VGAEModel(nn.Module):
+    def __init__(self, dimensions, adj, dropout):
+        super(VGAEModel, self).__init__()
+        self.adj = adj
+        self.gc1 = GraphConvolution(dimensions[0], dimensions[1], dropout, act=F.relu)
+        self.gc2 = GraphConvolution(dimensions[1], dimensions[2], dropout, act=lambda x: x)
+        self.gc3 = GraphConvolution(dimensions[1], dimensions[2], dropout, act=lambda x: x)
+
+    def encode(self, x):
+        hidden1 = self.gc1(x, self.adj)
+        return self.gc2(hidden1, self.adj), self.gc3(hidden1, self.adj)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(logvar)
+            eps = torch.randn_like(std)
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+class VGAE(ModelWithEmbeddings):
+
+    def __init__(self, output_dim=16, hiddens=None, max_degree=0, **kwargs):
+        if hiddens is None:
+            hiddens = [32]
+        super(VGAE, self).__init__(output_dim=output_dim, hiddens=hiddens, max_degree=max_degree, **kwargs)
+
+    @classmethod
+    def check_train_parameters(cls, **kwargs):
+        check_existance(kwargs, {"learning_rate": 0.1,
+                                 "epochs": 300,
+                                 "dropout": 0.,
+                                 "weight_decay": 1e-4,
+                                 "early_stopping": 100,
+                                 "clf_ratio": 0.8,
+                                 "hiddens": [32],
+                                 "max_degree": 0})
+        check_range(kwargs, {"learning_rate": (0, np.inf),
+                             "epochs": (0, np.inf),
+                             "dropout": (0, 1),
+                             "weight_decay": (0, 1),
+                             "early_stopping": (0, np.inf),
+                             "clf_ratio": (0, 1),
+                             "max_degree": (0, np.inf)})
+        return kwargs
+    
+    @classmethod
+    def check_graphtype(cls, graphtype, **kwargs):
+        if not graphtype.attributed():
+            raise TypeError("GAE only accepts attributed graphs!")
+
+    def build(self, graph, *, learning_rate=0.01, epochs=200,
+              dropout=0.5, weight_decay=1e-4, early_stopping=100,
+              clf_ratio=0.1, **kwargs):
+        """
+                        learning_rate: Initial learning rate
+                        epochs: Number of epochs to train
+                        hidden1: Number of units in hidden layer 1
+                        dropout: Dropout rate (1 - keep probability)
+                        weight_decay: Weight for L2 loss on embedding matrix
+                        early_stopping: Tolerance for early stopping (# of epochs)
+                        max_degree: Maximum Chebyshev polynomial degree
+        """
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.dropout = dropout
+        self.weight_decay = weight_decay
+        self.early_stopping = early_stopping
+        self.sparse = False
+
+        self.preprocess_data(graph)
+        # Create models
+        input_dim = self.features.shape[1] if not self.sparse else self.features[2][1]
+        feature_shape = self.features.shape if not self.sparse else self.features[0].shape[0]
+        self.dimensions = [input_dim]+self.hiddens+[self.output_dim]
+        self.model = VGAEModel(self.dimensions, self.support[0], self.dropout)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        
+
+    def train_model(self, graph, **kwargs):
+        # Train models
+        output, train_loss,  __ = self.evaluate(torch.ones(graph.nodesize))
+        self.debug_info = "train_loss = {:.5f}".format(train_loss)
+        return output
+        
+    def build_label(self, graph):
+        g = graph.G
+        look_up = graph.look_up_dict
+        labels = []
+        label_dict = {}
+        label_id = 0
+        for node in g.nodes():
+            labels.append((node, g.nodes[node]['label']))
+            for l in g.nodes[node]['label']:
+                if l not in label_dict:
+                    label_dict[l] = label_id
+                    label_id += 1
+        self.labels = torch.zeros((len(labels), label_id))
+        self.label_dict = label_dict
+        for node, l in labels:
+            node_id = look_up[node]
+            for ll in l:
+                l_id = label_dict[ll]
+                self.labels[node_id][l_id] = 1
+    
+    def loss(self, output, adj_label, pos_weight, norm, mu, logvar, n_nodes):
+        cost = 0.
+
+        cost += norm * F.binary_cross_entropy_with_logits(torch.mm(output, output.t()), adj_label, pos_weight=pos_weight)
+        KLD = -0.5 / n_nodes * torch.mean(torch.sum(1 + 2 * logvar - mu.pow(2) - logvar.exp().pow(2), 1))
+        return cost + KLD
+
+
+    # Define models evaluation function
+    def evaluate(self, mask, train=True):
+        #torch.autograd.set_detect_anomaly(True)
+        t_test = time.time()
+        self.optimizer.zero_grad()
+        self.model.train(train)
+        output, mu, var= self.model(self.features)
+
+        loss = self.loss(output, self.adj_label, self.pos_weight, self.norm, mu, var, self.n_nodes)
+        if train:
+            loss.backward()
+            #print([(name, param.grad) for name,param in self.model.named_parameters()])
+            self.optimizer.step()
+        return output, loss, (time.time() - t_test)
+
+    def make_output(self, graph, **kwargs):
+        output, mu, var= self.model(self.features)
+        self.embeddings = mu.detach()
+
+    def preprocess_data(self, graph):
+        """
+            adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask
+            y_train, y_val, y_test can merge to y
+        """
+        g = graph.G
+        look_back = graph.look_back_list
+        self.features = torch.from_numpy(graph.features()).type(torch.float32)
+        self.features = preprocess_features(self.features, sparse=self.sparse)
+
+        n = graph.nodesize
+        self.n_nodes = n
+        self.build_label(graph)
+        adj_label = graph.adjmat(weighted=False, directed=False, sparse=True)
+        
+        self.adj_label = torch.FloatTensor((adj_label + sp.eye(n).toarray()))
+        adj = nx.adjacency_matrix(g)  # the type of graph
+        self.pos_weight = torch.Tensor([float(n * n - adj.sum()) / adj.sum()])
+        self.norm = n * n / float((n * n - adj.sum()) * 2)
+
+        if self.max_degree == 0:
+            self.support = [preprocess_graph(adj)]
+        else:
+            self.support = chebyshev_polynomials(adj, self.max_degree)
+        # print(self.support)
+
+
+class GraphConvolution(nn.Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, dropout=0., act=F.relu):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.dropout = dropout
+        self.act = act
+        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, input, adj):
+        input = F.dropout(input, self.dropout, self.training)
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        output = self.act(output)
+        return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
